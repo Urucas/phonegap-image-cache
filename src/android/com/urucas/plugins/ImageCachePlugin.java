657064301687724d8b553f59bdc30b7a3f315318
Bruno Alassia <vruno.alassia@gmail.com>
2014-12-29 02:30:25 -0300

    Update ImageCachePlugin.java

--- src/android/com/urucas/plugins/ImageCachePlugin.java
+++ src/android/com/urucas/plugins/ImageCachePlugin.java
@@ -1,35 +1,48 @@
 package com.urucas.plugins;
 
-/*!
- *  
- * ToastPlugin.java
- * Created By Urucas
- * 
- * The source Code is available under 
- * same licenses as javamail which are:
- * CDDL-1.0, GPL-2.0, BSD
- * 
- */
+import java.lang.reflect.Method;
 
-import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaActivity;
+import org.apache.cordova.CordovaChromeClient;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
-import android.widget.Toast;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaWebViewClient;
 import android.util.Log;
 
 public class ImageCachePlugin extends CordovaPlugin {
 
-	@Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        
-			if(action.equals("load")) {
-				Log.i("load","yeap");				
-		  	return false;
-	    }	
-			return true;
+	private static final String TAG = "ImageCachePlugin";
+	private static ImageCache _cache;
+	private static ImageCachePlugin _instance;
+	
+	public ImageCachePlugin(){
+		_instance = this;
+	}
+	
+	public static ImageCache getCache() {
+		if(_cache == null){
+			_cache = new ImageCache(_instance.cordova.getActivity()); 
 		}
+		return _cache;
+	}
+	
+	public void initialize(CordovaInterface cordova, CordovaWebView webView){
+		super.initialize(cordova, webView);
+		
+		if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB ||
+                android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            	Log.i(TAG, "Android Platform not supported");
+        }
+        else {
+        	try {
+    			CordovaWebViewCached wbCached = new CordovaWebViewCached(this.cordova, webView);
+            		CordovaActivity _activity = (CordovaActivity) this.cordova.getActivity();
+            		_activity.init(webView, wbCached, webView.getWebChromeClient());
+    			
+    		} catch (Exception e) {
+    			Log.i("TAG", "Error reimplementing CordovaActivity");
+    		}
+        }
+	}
 }
